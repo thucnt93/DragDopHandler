9cf7fb85ad784b5996a49b4b115a5208c7c7bb5a
Thuc Nguyen <thucnt93@flomail.net>
2021-01-26 11:31:26 +0700

    Add drop drag tracking delegate

--- DragDropHandler/DragDropHandler/DropHandler/DropHandler.h
+++ DragDropHandler/DragDropHandler/DropHandler/DropHandler.h
@@ -8,10 +8,13 @@
 #import <Foundation/Foundation.h>
 #import "DragDropHandlerProtocol.h"
 #import "CustomDragOperation.h"
+#import "DragDropTrackingDelegate.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface DropHandler : NSObject<DestinationDropHandlerProtocol>
+
+- (instancetype)initWithDropTrackingDelegate:(id<DropTrackingDelegate>)delegate;
 
 @end
 
