79516a49d984baa784a09cf21ad45f90adf3ebcb
Thuc Nguyen <thucnt93@flomail.net>
2021-01-26 17:35:31 +0700

    fly test for view controller

--- DragDropHandler/Samples/Sources/ViewController.m
+++ DragDropHandler/Samples/Sources/ViewController.m
@@ -7,13 +7,17 @@
 //
 
 #import "ViewController.h"
+#import "DraggableNSButton.h"
+#import "TableViewManager.h"
+#import "DroppableNSView.h"
+#import "DraggableNSView.h"
 
-@interface ViewController() {
+@interface ViewController()<DragTrackingDelegate, DropTrackingDelegate> {
     
 }
-@property (weak) IBOutlet NSButton *fileDragButton;
+@property (weak) IBOutlet DraggableNSButton *fileDragButton;
 @property (weak) IBOutlet NSTableView *tableView;
-@property (weak) IBOutlet NSView *dropView;
+@property (weak) IBOutlet DroppableNSView *dropView;
 
 @end
 
@@ -23,25 +27,34 @@
     [super viewDidLoad];
 
     // Do any additional setup after loading the view.
+    [self setupDragDropTracking];
+    [self setupView];
+}
+
+- (void)setupView {
+    self.dropView.wantsLayer = YES;
+    self.dropView.layer.backgroundColor = [[NSColor systemYellowColor] CGColor];
+}
+
+- (void)setupDragDropTracking {
+    self.fileDragButton.dragTrackingDelegate = self;
+    self.dropView.dropTrackingDelegate = self;
     
 }
 
-- (void)addAutoResizingView:(NSView *)subView toView:(NSView *)containerView
-{
-    if (subView != nil)
-    {
-        [containerView addSubview:subView];
-        
-        [containerView setTranslatesAutoresizingMaskIntoConstraints:NO];
-        
-        [containerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[subView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(subView)]];
-        [containerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[subView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(subView)]];
-        
-        // Should force update view constraints for update UI correctly.
-        // [FM-1270] Show flashing for Flo app when open with Compact mode.
-        [containerView layoutSubtreeIfNeeded];
-        [containerView.superview layoutSubtreeIfNeeded];
-    }
+- (CustomDragOperation)dragBeginWithSource:(id)source atPoint:(NSPoint)atPoint {
+    return CustomDragOperation_LINK;
 }
+
+- (CustomDragOperation)dragMoveWithSource:(id)source atPoint:(NSPoint)atPoint {
+    return CustomDragOperation_LINK;;
+}
+
+- (CustomDragOperation)dragUpdatedOnTarget:(id)onTarget withInfo:(id<NSDraggingInfo>)draggingInfo {
+    
+    return CustomDragOperation_MOVE;
+}
+
+
 
 @end
