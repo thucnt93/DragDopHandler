e4fd7f919e8eea8b196d704d0397c28beaee686c
Thuc Nguyen <thucnt93@flomail.net>
2021-02-01 18:16:57 +0700

    Update DragDropTrackingDelegate.h

--- DragDropHandler/DragDropHandler/Protocol/DragDropTrackingDelegate.h
+++ DragDropHandler/DragDropHandler/Protocol/DragDropTrackingDelegate.h
@@ -28,6 +28,11 @@
 
 - (BOOL)tableViewAcceptDropOnTarget:(id)onTarget draggingInfo:(id<NSDraggingInfo>)info row:(NSInteger)row dropOperation:(NSTableViewDropOperation)dropOperation;
 
+#pragma mark TODO
+// Collection View:
+// Outline View:
+// Summanry method protocol for list view
+// Change name DragDropTrackingDelegate
 
 @end
 
